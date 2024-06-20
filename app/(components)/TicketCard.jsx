650825448365aa8ae6a27b88172042cd405fd057
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2024-06-20 19:31:22 -0300

    starting api development

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,10 +1,30 @@
-import React from "react";
 import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+      </div>
+      <div className="ml-auto">
+        <DeleteBlock />
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        this is the ticket description, please do this to solve the problem!
+      </p>
+      <div className="flex grow"></div>
+      <div className="flex flex-col">
+        <p className="text-xs my-1">10/11/24 13:50pm </p>
+        <ProgressDisplay />
+      </div>
+      <div className="ml-auto items-end">
+        <StatusDisplay />
+      </div>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,13 @@ import TicketCard from "./(components)/TicketCard";
 
 const Dashboard = () => {
   return (
-    <div>
-      <TicketCard />
+    <div className="p-5 ">
+      <div className="md:grid grid-cols-2 xl:grid-cols-4 xs:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
   );
 };
