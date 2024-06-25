a88dd417e0c4a980c466b7be2b6dc3564ea3135a
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2024-06-25 10:58:52 -0300

    ticket form

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -4,6 +4,20 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((previousState) => ({
+      ...previousState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    console.log("Submitted");
+  };
+
   //initial form
   const startingTicketData = {
     title: "",
@@ -16,7 +30,28 @@ const TicketForm = () => {
 
   //set initial form by the variable
   const [formData, setFormData] = useState(startingTicketData);
-  return <div>TicketForm</div>;
+
+  // form GUI that manages dynamic data
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create your ticket</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default TicketForm;
